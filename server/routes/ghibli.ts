1e900aac84803b99958e38b1ab0a9eb70b20eb70
Haruka Ogino <harukariq@gmail.com>
2024-03-27 09:23:48 +1300

    added some status on the backend

--- server/routes/ghibli.ts
+++ server/routes/ghibli.ts
@@ -17,7 +17,7 @@ router.get('/:id', async (req, res) => {
   const charId = req.params.id
   const char = await getCharacterById(charId)
   if (char) {
-    res.json(char)
+    res.status(200).json(char)
   } else {
     res.sendStatus(500)
   }
@@ -27,7 +27,7 @@ router.post('/add-char', async (req, res) => {
   try {
     const newChar = req.body
     await addNewChar(newChar)
-    res.send('New Character Added')
+    res.status(200).send('New Character Added')
   } catch (error) {
     console.error(error)
     res.sendStatus(500)
