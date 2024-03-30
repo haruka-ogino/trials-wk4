4771f0c4b90eeb144a294b2b2f9a208da19766eb
Haruka Ogino <harukariq@gmail.com>
2024-03-30 21:56:40 +1300

    auth config initialised

--- client/components/NewCharacter.tsx
+++ client/components/NewCharacter.tsx
@@ -1,5 +1,4 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { useParams } from 'react-router-dom'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { addChar } from '../api/charApi'
 import { useState } from 'react'
 
--- client/index.tsx
+++ client/index.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { routes } from './routes'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 const queryClient = new QueryClient()
 
@@ -11,8 +12,17 @@ const router = createBrowserRouter(routes)
 const root = createRoot(document.getElementById('app') as HTMLElement)
 
 root.render(
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-    <ReactQueryDevtools />
-  </QueryClientProvider>
+  <Auth0Provider
+    domain="harakeke24-haruka.au.auth0.com"
+    clientId="kKCmBrwLirAbQOl35xGciUNhPyakB1H2"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      audience: 'https://trials/api',
+    }}
+  >
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </Auth0Provider>
 )
